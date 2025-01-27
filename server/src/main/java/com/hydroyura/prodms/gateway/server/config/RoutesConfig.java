54b180e62ddfa9baa666b84ad51e0672955355a1
yury.klimchuk <hydro.yura@gmail.com>
2025-01-27 23:48:12 +0300

    changes

---
+++ server/src/main/java/com/hydroyura/prodms/gateway/server/config/RoutesConfig.java
@@ -0,0 +1,38 @@
+package com.hydroyura.prodms.gateway.server.config;
+
+import com.hydroyura.prodms.gateway.server.model.api.ApiRes;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class RoutesConfig {
+
+    @Autowired
+    private TestRewriteFunction test;
+
+    @Bean
+    RouteLocator getUnitRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(
+                "units-get",
+                r -> r
+                    .path("/api/v1/units/{number}")
+                    .and()
+                    .method(HttpMethod.GET)
+                    .filters( f -> f.modifyResponseBody(ApiRes.class, ApiRes.class, test))
+                    .uri("http://localhost:8081"))
+            .build();
+    }
+
+    public <T> Class<ApiRes<T>> getClass(Class<ApiRes<T>> clazz) {
+        return clazz;
+    }
+
+
+
+}
---
+++ server/src/main/java/com/hydroyura/prodms/gateway/server/config/TestRewriteFunction.java
@@ -0,0 +1,33 @@
+package com.hydroyura.prodms.gateway.server.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hydroyura.prodms.gateway.server.model.api.ApiRes;
+import java.util.Map;
+import lombok.SneakyThrows;
+import org.reactivestreams.Publisher;
+import org.springframework.cloud.gateway.filter.factory.rewrite.RewriteFunction;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class TestRewriteFunction implements RewriteFunction<ApiRes, ApiRes> {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @SneakyThrows
+    @Override
+    public Publisher<ApiRes> apply(ServerWebExchange serverWebExchange, ApiRes apiRes) {
+        Map data = objectMapper.readValue(objectMapper.writeValueAsString(apiRes.getData()), Map.class);
+        data.put("links", Map.of("link", "ahref", "link2", "ahgd"));
+        apiRes.setData(data);
+        
+
+
+
+
+        return Mono.just(apiRes);
+    }
+
+}

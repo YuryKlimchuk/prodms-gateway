c668233c49dac54bd2315d6d3499b0901af58253
yury.klimchuk <hydro.yura@gmail.com>
2025-01-30 23:35:48 +0300

    changes

--- server/src/main/java/com/hydroyura/prodms/gateway/server/config/TestRewriteFunction.java
+++ server/src/main/java/com/hydroyura/prodms/gateway/server/config/TestRewriteFunction.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.hydroyura.prodms.archive.client.model.api.ApiRes;
+import com.hydroyura.prodms.archive.client.model.res.GetUnitRes;
 import java.util.Map;
 import lombok.SneakyThrows;
 import org.reactivestreams.Publisher;
@@ -20,8 +21,8 @@ import reactor.core.publisher.Mono;
 @Component
 public class TestRewriteFunction implements RewriteFunction<ApiRes, ApiRes> {
 
-    @Value("${microservices.urls.files}")
-    private String filesUrl;
+    //@Value("${microservices.urls.files}")
+    private String filesUrl = "http://localhost:8085";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final JavaType mapType = TypeFactory
@@ -33,21 +34,26 @@ public class TestRewriteFunction implements RewriteFunction<ApiRes, ApiRes> {
     public Publisher<ApiRes> apply(ServerWebExchange serverWebExchange, ApiRes apiRes) {
         Map<String, Object> data = objectMapper.readValue(objectMapper.writeValueAsString(apiRes.getData()), mapType);
 
-        ResponseEntity<ApiRes> urls =  WebClient.builder()
+        Mono<ApiRes> resMono =  WebClient.builder()
             .baseUrl(filesUrl)
             .build()
             .method(HttpMethod.GET)
             .uri("/api/v1/units/{number}", data.get("number").toString())
             .contentType(MediaType.APPLICATION_JSON)
             .retrieve()
-            .toEntity(ApiRes.class)
-            .block();
+            .bodyToMono(ApiRes.class);
 
-
+        //resMono.subscribe(System.out::println);
 
 
         return Mono.just(apiRes);
     }
+
+    private Mono<ApiRes<GetUnitRes>> getCurrentResponse() {
+        return Mono.empty();
+    }
+
+    private Mono<ApiRes<Get>>
 
 
 }
